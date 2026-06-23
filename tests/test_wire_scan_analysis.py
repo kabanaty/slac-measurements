dfd884a18f8b4c3e06834086ca1e7fcef5354218
Tyler Kabana <tylerkabana@gmail.com>
2026-06-23 10:23:32 -0700

    Fix import paths for WireMeasurementCollectionResult and WireMeasurementAnalysisResult classes

--- tests/test_wire_scan_analysis.py
+++ tests/test_wire_scan_analysis.py
@@ -5,7 +5,7 @@ from unittest import TestCase
 from unittest.mock import patch
 
 from slac_measurements.wires.analysis import WireMeasurementAnalysis
-from slac_measurements.wires.analysis_results import (
+from slac_measurements.wires.analysis.results import (
     DetectorFit,
     DetectorProfileMeasurement,
     FitResult,
