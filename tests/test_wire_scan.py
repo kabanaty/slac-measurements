6cd38688a8f43123932cfba2c5fbf5396b1a481d
Tyler Kabana <tylerkabana@gmail.com>
2026-08-21 11:10:24 -0700

    Add jitter BPMs parameter to analysis call in WireBeamProfileMeasurementTest

--- tests/test_wire_scan.py
+++ tests/test_wire_scan.py
@@ -159,6 +159,7 @@ class WireBeamProfileMeasurementTest(TestCase):
         mock_analysis_cls.return_value.analyze.assert_called_once_with(
             rms_detector="D2",
             jitter_correction=False,
+            jitter_bpms=None,
         )
 
     def test_collection_result_round_trip_preserves_buffer_number(self):
